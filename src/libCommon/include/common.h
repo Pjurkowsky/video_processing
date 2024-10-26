9521ec04a5cb26aa9a4b9bd86cfb71eb48083908
anatorini <marcin.szymczak156@gmail.com>
2024-10-26 18:37:28 +0200

    Add `benchmark` utility for measuring function execution time

--- src/libCommon/include/common.h
+++ src/libCommon/include/common.h
@@ -1,3 +1,8 @@
 #pragma once
+#include <chrono>
+#include <functional>
+#include "video.h"
 
 void common();
+
+void benchmark(const std::function<void()>& f);
--- src/libCommon/src/common.cpp
+++ src/libCommon/src/common.cpp
@@ -1,6 +1,17 @@
 #include <common.h>
+#include <chrono>
+#include <functional>
 #include <iostream>
 
 void common() {
   std::cout << "Hello from the Common library" << std::endl;
+}
+
+void benchmark(const std::function<void()>& f) {
+  auto start = std::chrono::steady_clock::now();
+  f();
+  auto end = std::chrono::steady_clock::now();
+  std::cout << (end - start) / std::chrono::milliseconds(1) << " ms"
+            << std::endl;
+  ;
 }
